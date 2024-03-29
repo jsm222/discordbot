8794e07ebb9246a106da35e835c02752a7f6d3dc
Cait Himes <cait.himes@outlook.com>
2024-03-29 16:35:33 -0400

    finish fixing role command

--- commands/utlity/role.js
+++ commands/utlity/role.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { assignable_roles } = require("../../config.json");
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("role")
@@ -26,23 +27,36 @@ module.exports = {
   async execute(interaction) {
     // set error text in case of an issue
     let responseText = "Unknown or invalid role.";
-    // fetch the role ids defined in config.json
-    let allowedRoleIDs = assignable_roles.map(i => i.id);
     // get the role id the user requested
-    const askedRoleID = interaction.options.getRole('role');
+    const askedRoleName = interaction.options.getString('role');
     // is this role allowed to be assigned?
-    if (allowedRoleIDs.findIndex((s) => askedRoleID == s) >= 0) {
+    if (assignable_roles.findIndex((s) => askedRoleName == s.name) >= 0) {
       // CASE: found role in allowed roles list
+      // convert the role name to a role id
+      const askedRoleID = assignable_roles.find((role) => role.name == askedRoleName).id;
       // does the member already have the role?
       if (interaction.member.roles.cache.some(role => role.id == askedRoleID)) {
         // CASE: the member has the role, so remove it
         interaction.guild.members.cache.get(interaction.member.id).roles.remove(askedRoleID);
-        responseText = `Removed ${askedRoleID} role from ${interaction.member}.`;
+        responseText = `Removed <@&${askedRoleID}> role from ${interaction.member}.`;
       }
       else {
         // CASE: the member does not have the role, so add it
+        // reset response text
+        responseText = "";
+        // remove any conflicting roles
+        const askedRoleGroup = assignable_roles.find(role => role.name == askedRoleName).group;
+        const conflictingRoleIDs = assignable_roles.filter(role => role.group == askedRoleGroup).map(role => role.id);
+        for (let i in conflictingRoleIDs) {
+          let conflictingRoleID = conflictingRoleIDs[i];
+          if (interaction.member.roles.cache.some(role => role.id == conflictingRoleID)) {
+            interaction.guild.members.cache.get(interaction.member.id).roles.remove(conflictingRoleID);
+            responseText += `Removed conflicting <@&${conflictingRoleID}> role from ${interaction.member}.\n`;
+          }
+        }
+        // add requested role
         interaction.guild.members.cache.get(interaction.user.id).roles.add(askedRoleID);
-        responseText = `Added ${askedRoleID} role to ${interaction.member}.`;
+        responseText += `Added <@&${askedRoleID}> role to ${interaction.member}.`;
       }
     }
     // inform user on execution status
